97ecc00afeb94495718269bc8ad8f9739f1a5995
kakachan <chen-kaka@163.com>
2017-02-15 13:28:13 +0800

    net data admin

--- Model/admin.py
+++ Model/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from Model.models import Test,Contact,Tag
-from Model.mutualfund import FundFinance,FundScale,FundConfig
+from Model.mutualfund import FundFinance,FundScale,FundConfig,FundNetData
 
 # Register your models here.
 class TagInline(admin.TabularInline):
@@ -31,6 +31,9 @@ class FundScaleAdmin(admin.ModelAdmin):
     list_display = ('code','name','time') # list
 class FundConfigAdmin(admin.ModelAdmin):
         list_display = ('code','name','time') # list
+class FundNetDataAdmin(admin.ModelAdmin):
+    list_display = ('symbol','sname','nav_date','jjlx') # list
 admin.site.register(FundFinance, FundFinanceAdmin)
 admin.site.register(FundScale, FundScaleAdmin)
 admin.site.register(FundConfig, FundConfigAdmin)
+admin.site.register(FundNetData, FundNetDataAdmin)
