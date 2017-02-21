75342cba872c069410c46fd911797225f15ea682
kakachan <chen-kaka@163.com>
2017-02-21 17:10:00 +0800

    lxml解析

--- DataCrawler/Requester.py
+++ DataCrawler/Requester.py
@@ -2,6 +2,7 @@
 
 import requests
 from bs4 import BeautifulSoup
+import lxml
 
 def requestData(reqUrl):
     responseHtml = requests.get(reqUrl)
@@ -16,10 +17,24 @@ def fetchMutualFundRatingData():
     pagesize = 20
     reqUrl = url + "&date=" + date + "&pageindex=" + bytes(pageindex) + "&pagesize=" + bytes(pagesize)
     print "reqUrl is:", reqUrl
-    responseHtml = requestData(reqUrl)
+    # responseHtml = requestData(reqUrl)
     # print "responseHtml is: ", responseHtml
-    soup = BeautifulSoup(responseHtml.text, "html.parser")
-    print soup.prettify()
-    targetTable = soup.html.body
+    tt = open("/Users/kakachan/Desktop/fundranking.ashx.htm");
+    # soup = BeautifulSoup(responseHtml.text, "html.parser")
+    soup = BeautifulSoup(tt, "lxml")
+
+    # print soup.prettify()
+    targetTable = soup.findAll("table",class_="fr_tablecontent")
+    # print targetTable
+    trs = targetTable[0].findAll('tr')
+    for index in range(0,len(trs)-1):
+        tr = trs[index]
+        tds = tr.findAll('td')
+        print "len: ", len(tds)
+        for i in range(1,(len(tds))):
+            td = tds[i]
+            print td.get_text()
+
+    # tr = soup.find('tr',attrs={"onmouseout": "this.style.background=''"})
 
 fetchMutualFundRatingData()
